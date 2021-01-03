9197cf53dc8f2d5f20acf9d449249ba56adaef81
Rafiq Kamal <71498824+MeasureTwiceCutOnce@users.noreply.github.com>
2021-01-03 08:49:13 +0300

    finished missing numbers problem

--- missingNumber.js
+++ missingNumber.js
@@ -1,11 +1,26 @@
 // Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 
-Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+// Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
 
 var missingNumber = function(nums) {
-  const set = new Set();
+  let sumIfNotMissing = nums.length;
+  let actualSum = 0;
 
-  for(const num of nums) {
-    set[num] = 
+  for (let i = 0; i < nums.length; i++) {
+    sumIfNotMissing += i;
+    actualSum += nums[i];
   }
-};
+  
+  return sumIfNotMissing - actualSum;
+}
+
+//   const set = new Set();
+
+//   for(const num of nums) {
+//     if (num.indexOf() === -1) {
+//       return num;
+//     }
+//   }
+// };
+
+// console.log(missingNumber([3,0,1]))
