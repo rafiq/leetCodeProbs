61799ceee70993fc4ed9cc9db9d7fef7c2b38969
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-01-11 08:10:14 +0300

    Update removeDuplicatesFromSortedArray.js

--- removeDuplicatesFromSortedArray.js
+++ removeDuplicatesFromSortedArray.js
@@ -1,28 +1,28 @@
-// Remove Duplicates from Sorted Array
+// // Remove Duplicates from Sorted Array
 
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var removeDuplicates = function(nums) {
-  if (nums.length === 0) {
-    return 0;
-  }
-  let pointer1 = 0;
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var removeDuplicates = function(nums) {
+//   if (nums.length === 0) {
+//     return 0;
+//   }
+//   let pointer1 = 0;
 
-  for (let pointer2 = 1; pointer2 < nums.length; pointer2++) {
-    if(nums[pointer1] !== nums[pointer2]) {
-      pointer1++;
-      nums[pointer1] =nums[pointer2];
-    }
+//   for (let pointer2 = 1; pointer2 < nums.length; pointer2++) {
+//     if(nums[pointer1] !== nums[pointer2]) {
+//       pointer1++;
+//       nums[pointer1] = nums[pointer2];
+//     }
 
-  }
+//   }
 
-  return pointer1 + 1;
-};
+//   return pointer1 + 1;
+// };
 
-console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
-console.log(removeDuplicates([1,1,2]));
+// console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
+// console.log(removeDuplicates([1,1,2]));
 // Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
 
 // Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
