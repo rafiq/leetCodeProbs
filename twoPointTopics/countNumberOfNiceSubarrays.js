4aa3b0aec6e923d4dee53f4e0691c7a776b28233
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-01-23 08:05:06 +0300

    Create countNumberOfNiceSubarrays.js

---
+++ twoPointTopics/countNumberOfNiceSubarrays.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numberOfSubarrays = function(nums, k) {
+    let count = 0;
+
+    if(nums.forEach(num => num % 2 === 0)) count = 0;
+
+    let i = 0;
+    let j = 0;
+
+    while()
+
+    return count;
+};
+
+console.log(numberOfSubarrays([1,1,2,1,1],3))
+// Given an array of integers nums and an integer k. A continuous subarray is called nice if there are k odd numbers on it.
+
+// Return the number of nice sub-arrays.
