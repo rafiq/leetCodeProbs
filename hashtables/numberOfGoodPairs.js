428f04577dff53e09ce7861666200a270f12e050
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-01-29 18:48:22 +0300

    Create numberOfGoodPairs.js

---
+++ hashtables/numberOfGoodPairs.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function(nums) {
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                count++;
+            }
+        }
+    }
+    return count;
+};
+console.log(numIdenticalPairs([1,2,3,1,1,3]));
+console.log(numIdenticalPairs([1,1,1,1]));
+console.log(numIdenticalPairs([1,2,3]));
+
+// Given an array of integers nums.
+
+// A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+
+// Return the number of good pairs.
+
