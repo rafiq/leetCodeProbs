16068cc061b03828ced13e3d65e8234cdf6da252
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-02-07 19:52:47 +0300

    Update linkedListCycle.js

--- twoPointTopics/linkedListCycle.js
+++ twoPointTopics/linkedListCycle.js
@@ -11,6 +11,22 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
+    let i = head;
+    let j = head;
 
+    while (i !== null && i.next !== null) {
+        i = i.next.next;
+        j = j.next;
+        if (i === j) {
+            return true;
+        }
+    }
+    return false;
 };
 
+console.log(hasCycle([3,2,0,-4]));
+// Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+// Return true if there is a cycle in the linked list. Otherwise, return false.
