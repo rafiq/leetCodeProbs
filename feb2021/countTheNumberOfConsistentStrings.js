63b6487d56ce60d088d3b272e099499736082bb4
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-02-13 17:36:56 +0300

    Update countTheNumberOfConsistentStrings.js

--- feb2021/countTheNumberOfConsistentStrings.js
+++ feb2021/countTheNumberOfConsistentStrings.js
@@ -4,17 +4,21 @@
  * @return {number}
  */
 var countConsistentStrings = function(allowed, words) {
-    let chars = allowed.split("");
-    let count = [];
+    let charsAllowed = new Set(allowed)
+    let count = 0;
 
     for (let word of words) {
-        if (word.split("").includes(!chars)) {
-            continue;
-        }
-        return count.push(word);
+        count += isValid(word, charsAllowed);
     }
-
-    return count.length;
+    return count;
+    function isValid(string) {
+        for (let char of string) {
+            if (!charsAllowed.has(char)) {
+                return false;
+            }
+        }
+        return true;
+    }
 };
 console.log(
     countConsistentStrings("ab",["ad","bd","aaab","baa","badab"]), //2
@@ -22,6 +26,26 @@ console.log(
     countConsistentStrings("cad",["cc","acd","b","ba","bac","bad","ac","d"]),//4
 
 )
+// My solution below kept returning one for each test case, and I think it is due to the char set that I am checking. It may need to be split up or something done to it, to make it check for each letter with each letter in the array.
+// var countConsistentStrings = function(allowed, words) {
+//     let chars = allowed.split("");
+//     let count = [];
+
+//     for (let word of words) {
+//         if (word.split("").includes(!chars)) {
+//             continue;
+//         }
+//         return count.push(word);
+//     }
+
+//     return count.length;
+// };
+// console.log(
+//     countConsistentStrings("ab",["ad","bd","aaab","baa","badab"]), //2
+//     countConsistentStrings("abc",["a","b","c","ab","ac","bc","abc"]), //7
+//     countConsistentStrings("cad",["cc","acd","b","ba","bac","bad","ac","d"]),//4
+
+// )
 // 1684. Count the Number of Consistent Strings
 // You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
 
