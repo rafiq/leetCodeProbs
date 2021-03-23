275d009b3db0a1aeeeda35350c1b57f62eec80a0
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-03-23 17:28:18 +0300

    Update nimGame.js

--- Mar2021/nimGame.js
+++ Mar2021/nimGame.js
@@ -2,13 +2,24 @@
  * @param {number} n
  * @return {boolean}
  */
-var canWinNim = function(n) {
+// var canWinNim = function(n) {
 
-    if (n === 1) return true;
-    return n % 4 === 0 ? false : true;
-};
-console.log(
-    canWinNim(4),//false;
-    canWinNim(2),//true
+//     if (n === 1) return true;
+//     return n % 4 === 0 ? false : true;
+// };
+// console.log(
+//     canWinNim(4),//false;
+//     canWinNim(2),//true
 
-)
+// )
+
+function turnOn(machine) {
+    return machine.
+      isOn = true
+
+}
+var computer = {
+    isOn : false                                                     }
+console.log(computer)
+console.log(turnOn(computer));
+console.log(computer)
