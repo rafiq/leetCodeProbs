0d9b65f5b7feef2ce74c6a694a5cdcff7034fc93
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-05-31 21:07:52 +0300

    Create countGoodTriplets.js

---
+++ Apr2021/countGoodTriplets.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+ var countGoodTriplets = function(arr, a, b, c) {
+
+};
+console.log(
+    countGoodTriplets([3,0,1,1,9,7],7,2,3),//4
+)
