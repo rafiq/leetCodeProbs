54cb5114101b63ec4e2dac248f7f4e6f65a4a2b7
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-07-10 20:46:41 +0300

    Create bouncingBeams.js

---
+++ Mar2021/bouncingBeams.js
@@ -0,0 +1,22 @@
+function exit_from_maze(board) {
+    let res = {
+        position: [0, 0],
+        distance: 0
+      };
+
+    return res.position = [1,1]
+  }
+
+console.log(
+    exit_from_maze([
+        "##############",
+            "#        \\   #",
+            "*   \\        #",
+            "#            #",
+            "#   \\    /   #",
+            "##############"
+      ]),//{
+    //     position: [0, 0],
+    //     distance: 0
+    //   };
+    )
