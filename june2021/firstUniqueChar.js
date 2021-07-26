39681d3ac8e57b9dd60cdf6e7ab7ecdbe8e0e295
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-07-25 21:42:28 -0700

    Create firstUniqueChar.js

---
+++ june2021/firstUniqueChar.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var firstUniqChar = function(s) {
+
+    for (let i = 0; i < s.length; i++) {
+        if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) return i
+    }
+    return -1
+};
+console.log(
+    firstUniqChar("loveleetcode"),//2
+    firstUniqChar("leetcode"),//0
+    firstUniqChar("aabb"),//-1
+)
