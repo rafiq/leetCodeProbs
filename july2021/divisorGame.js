f557eb763557101bf02e3448658215a2112e3001
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-09-11 05:05:56 +0300

    Create divisorGame.js

---
+++ july2021/divisorGame.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+ var divisorGame = function(n) {
+    return n % 2 === 0
+
+    // if (n === 0) return true
+
+    // const isValid = num => {
+    //     return num < n && n % num === 0
+    // }
+
+    // if (isValid(n)) {
+    //     return divisorGame(n - x)
+    // }
+};
+console.log(
+    divisorGame(2),//true
+
+)
