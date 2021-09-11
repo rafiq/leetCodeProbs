4423754af63d1170af2c58bbeac938569e76066f
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-09-11 05:06:15 +0300

    Create concatArray.js

---
+++ aug2021/concatArray.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var getConcatenation = function(nums) {
+    return nums.slice().concat(nums)
+};
+console.log(
+    getConcatenation([1,2,1])
+)
