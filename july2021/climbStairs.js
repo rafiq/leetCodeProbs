774aedc4f2f5930bcb3a39fc1d15e8600f3fec5e
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-09-17 18:43:00 -1100

    Create climbStairs.js

---
+++ july2021/climbStairs.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var climbStairs = function(n) {
+    let dp = [1,1]
+
+    for (let i = 2; i <= n; i++) {
+        let result = dp[0] + dp[1]
+        dp[0] = dp[1]
+        dp[1] = result
+    }
+
+    return dp[1]
+};
+console.log(
+    climbStairs(3),//
+)
