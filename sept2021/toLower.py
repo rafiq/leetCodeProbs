700647eecbb761697aedd486e4e38a77d98ed5b4
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-10-25 18:31:17 +0300

    Create toLower.py

---
+++ sept2021/toLower.py
@@ -0,0 +1,9 @@
+class Solution:
+    def toLowerCase(self, str):
+        return "".join(chr(ord(c) + 32) if 65 <= ord(c) <= 90 else c for c in str)
+class Solution:
+    def toLowerCase(self, str):
+        return "".join(chr(ord(c) + 32) if "A" <= c <= "Z" else c for c in str)
+class Solution:
+    def toLowerCase(self, str):
+        return str.lower()
