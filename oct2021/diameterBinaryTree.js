2b464fb42b8152cf01d3c24e7df1152b570273dc
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-12-23 18:24:28 +0300

    Create diameterBinaryTree.js

---
+++ oct2021/diameterBinaryTree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var diameterOfBinaryTree = function(root) {
+
+
+    return dfs;
+    function dfs(root) {
+        if (!root) return null;
+        let maxNodeRoute = 0;
+
+
+
+    }
+};
+console.log(
+    diameterOfBinaryTree([1,2,3,4,5]),//3
+);
