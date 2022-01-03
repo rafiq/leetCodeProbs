47dd282809571c69b90a36c11d443198171ae3a5
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2022-01-03 18:42:40 +0300

    Create binaryTreePath.js

---
+++ oct2021/binaryTreePath.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+ var binaryTreePaths = function(root,path) {
+    // let path = [];
+    // if (root) {
+    //     path += String(root.val);
+
+    //     if (!root.left && !root.right) {
+    //         paths.push(path)
+    //     } else {
+    //         path += "->";
+    //     }
+    // }
+    // let paths = [];
+    // binaryTreePaths(root,"");
+    // return paths;
+
+    let paths = [];
+
+function dfsTraversal(root, cur) {
+    if (!root) return;
+    if (!root.left && !root.right) {
+        paths.push(cur + root.val);
+        return;
+    }
+    dfsTraversal(root.left, cur + root.val + "->");
+    dfsTraversal(root.right, cur + root.val + "->");
+}
+
+dfsTraversal(root, "");
+return paths;
+};
+
