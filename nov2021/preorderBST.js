5daaa94d1a92192c620da0825379133230b00ff5
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2022-01-20 18:59:11 +0300

    Create preorderBST.js

---
+++ nov2021/preorderBST.js
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+
+
+
+var preorder = function(root) {
+    let res = [];
+    function dfsTraversal(root) {
+        if (!root) return;
+        res.push(root.val);
+        for (let child of root.children) {
+            dfsTraversal(child);
+        }
+    }
+    dfsTraversal(root);
+    return res;
+    // Time Complexity: O(N)
+    // Space Complexity: O(H)
+};
+// Iterative
+var preorder = function(root) {
+    if (!root) return [];
+    let stack = [root], res = [];
+    while (stack.length > 0) {
+        let node = stack.pop();
+        res.push(node.val);
+        for (let i = node.children.length - 1; i >= 0; i--) {
+            if (node.children[i]) stack.push(node.children[i]);
+        }
+    }
+    return res;
+    // Time Complexity: O(N)
+    // Space Complexity: O(H)
+}
