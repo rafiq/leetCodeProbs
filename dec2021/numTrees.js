e6b485f68c740246d38f7b6c8763dfcc867f76b9
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2022-03-17 17:51:11 +0300

    Create numTrees.js

---
+++ dec2021/numTrees.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var numTrees = function(n) {
+    let dp = new Array(n + 1).fill(1);
+    for (let i = 2; i <= n; i++) {
+      let count = 0;
+      for (let j = 1; j <= i; j++) {
+        count += dp[j - 1] * dp[i - j];
+      }
+      dp[i] = count;
+    }
+    return dp[n];
+};
+console.log(
+    numTrees()
+);
