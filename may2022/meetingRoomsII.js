6985d832fff1acb3873bef93deb90d30fc3db182
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2023-01-31 16:40:56 -0600

    Create meetingRoomsII.js

---
+++ may2022/meetingRoomsII.js
@@ -0,0 +1,19 @@
+var minMeetingRooms = function(intervals) {
+    let starts = intervals.map(interval => interval[0]);
+    let ends = intervals.map(interval => interval[1]);
+    starts.sort((a,b) => a-b);
+    ends.sort((a,b) => a-b);
+    let count = 0, j = 0;
+    for (let i = 0; i < starts.length; i++) {
+        count++;
+        // a meeting ended and its room became available
+        if (starts[i] >= ends[j]) {
+            count--;
+            j++
+        }
+    }
+    return count;
+    // T.C: O(Nlog(N))
+    // S.C: O(N)
+};
+
